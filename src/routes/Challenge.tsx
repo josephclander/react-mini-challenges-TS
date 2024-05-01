57c3758792ed2dd67cf990fb7d61582db7a74774
josephclander <josephclander@gmail.com>
2024-05-01 14:17:39 +0100

    Add Challenge CSS

--- src/routes/Challenge.tsx
+++ src/routes/Challenge.tsx
@@ -2,6 +2,7 @@ import { Navigate, useParams } from "react-router-dom";
 
 import challenges from "../assets/challenges.json";
 import solutionComponents from "../solutions/SolutionIndex";
+import styles from "./Challenge.module.css";
 
 const Challenge = () => {
   const { id } = useParams<{ id?: string }>();
@@ -17,11 +18,11 @@ const Challenge = () => {
   }
 
   return (
-    <main>
-      <h1>CHALLENGE {id}</h1>
-      <h2>{challenge.title}</h2>
-      <p>{challenge.details}</p>
-      <hr />
+    <main className={styles.Challenge__container}>
+      <h1 className={styles.Challenge__heading}>CHALLENGE {id}</h1>
+      <h2 className={styles.Challenge__title}>{challenge.title}</h2>
+      <p className={styles.Challenge__details}>{challenge.details}</p>
+      <hr className={styles.Challenge__divider} />
       {SolutionComponent ? <SolutionComponent /> : <p>No solution available</p>}
     </main>
   );
